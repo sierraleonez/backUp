fd5dd1aef62349cab27238dfa303e3a8fb65caa6
sierraleonez <2171364@gmail.com>
2021-02-24 21:51:55 +0700

    Init Project

---
+++ index.js
@@ -0,0 +1,20 @@
+var readBooks = require("./callback.js");
+
+var books = [
+  { name: "LOTR", timeSpent: 3000 },
+  { name: "Fidas", timeSpent: 2000 },
+  { name: "Kalkulus", timeSpent: 4000 },
+  { name: "komik", timeSpent: 1000 },
+];
+
+// Tulis code untuk memanggil function readBooks di sini
+readBooks(10000, books[0], (sisa) => {
+  readBooks(sisa, books[1], (sisaBook1) => {
+    readBooks(sisaBook1, books[2], (sisaBook2) => {
+      readBooks(sisaBook2, books[3], (sisaBook3) => {
+        console.log(sisaBook3);
+      });
+    });
+  });
+});
+// lanjutkan code pada index.js untuk memanggil function readBooks. Buku yang akan dihabiskan adalah buku-buku di dalam array books. Pertama function readBooks menerima input waktu yang dimiliki yaitu 10000 ms (10 detik) dan books pada indeks ke-0. Setelah mendapatkan callback sisa waktu yang dikirim lewat callback, sisa waktu tersebut dipakai untuk membaca buku pada indeks ke-1. Begitu seterusnya sampai waktu habis atau semua buku sudah terbaca
